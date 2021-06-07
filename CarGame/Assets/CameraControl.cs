1c2beaa036e3dd5342d71897a27ef770c46e4110
vian0nguyen <vian0nguyen@gmail.com>
2021-06-07 19:59:15 -0400

    Setting up work for MouseInteraction script

--- CarGame/Assets/CameraControl.cs
+++ CarGame/Assets/CameraControl.cs
@@ -7,6 +7,7 @@ public class CameraControl : MonoBehaviour
 	//bless reddit https://www.reddit.com/r/Unity3D/comments/8k7w7v/unity_simple_mouselook/
 	Vector2 rotation = Vector2.zero; //zero out the rotation
 	public float speed = 3;
+	//NOTE, the floats don't map at all to the transform units in game, jsyk
 	public float clampUp = 5f;
 	public float clampDown = -2f;
 	public float clampLeft = 8f;
---
+++ CarGame/Assets/MouseInteract.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInteract : MonoBehaviour
+{
+
+    //TODO: set up a raycast mapped to mouse movement and be able to interact with objects
+    //https://gamedevbeginner.com/how-to-convert-the-mouse-position-to-world-space-in-unity-2d-3d/#screen_to_world_3d
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
