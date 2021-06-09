2edb9081711c29d94ea6fba98fda620d975aed4a
vian0nguyen <vian0nguyen@gmail.com>
2021-06-08 20:55:46 -0400

    Included basic mouse based raycast interaction script and began car prototype
    TODO: get some feedback attached to mouse movement and clicking, maybe add a roof............. gotta get an understanding of height :thinkies:

---
+++ CarGame/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+	//TODO: get mouse look to be mapped to the area of the window and not the area of the display?
+	//bless reddit https://www.reddit.com/r/Unity3D/comments/8k7w7v/unity_simple_mouselook/
+	Vector2 rotation = Vector2.zero; //zero out the rotation
+	public float speed = 3;
+	//NOTE, the floats don't map at all to the transform units in game, jsyk
+	public float clampUp = 5f;
+	public float clampDown = -2f;
+	public float clampLeft = 8f;
+	public float clampRight = -8f;
+
+	void Update()
+	{
+		rotation.y += Input.GetAxis("Mouse X"); //maps the rotation y to 
+		rotation.x += -Input.GetAxis("Mouse Y");
+		rotation.x = Mathf.Clamp(rotation.x, clampDown, clampUp); //clamps the up/down position
+		rotation.y = Mathf.Clamp(rotation.y, clampLeft, clampRight);
+		transform.eulerAngles = (Vector2)rotation * speed;
+	}
+}
---
+++ CarGame/Assets/Scripts/MouseInteract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInteract : MonoBehaviour
+{
+    
+
+    //TODO: set up a raycast mapped to mouse movement and be able to interact with objects
+    //https://gamedevbeginner.com/how-to-convert-the-mouse-position-to-world-space-in-unity-2d-3d/#screen_to_world_3d
+    void Update()
+    {
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitData;
+
+
+        if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "Interactive")
+        {
+            Debug.Log("something Interactive");
+        }
+
+        if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "Object")
+        {
+            Debug.Log("an Object");
+        }
+    }
+}
