75a6c7bee26a55a961d90d9be9e4ddb1362c3f90
vian0nguyen <vian0nguyen@gmail.com>
2021-06-15 22:05:05 -0400

    new system architecture and proto code to do something when clicking objects!!

---
+++ CarGame/Assets/Scripts/ClickObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickObject : MonoBehaviour
+{
+    public string clickObject;
+    public Renderer objRenderer;
+    public Color objectColorChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objRenderer = gameObject.GetComponent<Renderer>();
+    }
+   
+}
--- CarGame/Assets/Scripts/MouseInteract.cs
+++ CarGame/Assets/Scripts/MouseInteract.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class MouseInteract : MonoBehaviour
 {
-    
+    public string clickingOn;
 
-    //TODO: set up a raycast mapped to mouse movement and be able to interact with objects
     //https://gamedevbeginner.com/how-to-convert-the-mouse-position-to-world-space-in-unity-2d-3d/#screen_to_world_3d
     void Update()
     {
@@ -14,15 +13,38 @@ public class MouseInteract : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitData;
 
-
-        if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "Interactive")
+        //use this area to set up categories of items to be clicked on
+        //REMEMBER: object needs to be tagged + spelled correctly (capsSensitive!)
+        //probably needs a collider too
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("something Interactive");
+            if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "Interactive")
+            {
+                clickingOn = "Interactive"; //sets the public string to be "interactive"!
+                ClickObject clickedObject = hitData.transform.gameObject.GetComponent<ClickObject>();
+                printObjectStats(clickedObject);
+
+                Debug.Log("something Interactive");
+            }
+
+            if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "Object")
+            {
+                clickingOn = "Object";
+                ClickObject clickedObject = hitData.transform.gameObject.GetComponent<ClickObject>();
+                printObjectStats(clickedObject);
+
+                Debug.Log("an Object");
+            }
+
         }
 
-        if (Physics.Raycast(ray, out hitData, 1000) && hitData.transform.tag == "Object")
+        void printObjectStats(ClickObject clickObj)
         {
-            Debug.Log("an Object");
+            print(clickObj.name);
+            print(clickObj.objRenderer);
+            clickObj.objRenderer.material.color = clickObj.objectColorChange; //changes object color
+
         }
+
     }
 }
